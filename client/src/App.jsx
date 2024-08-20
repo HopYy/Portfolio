cbe014261e72ba6c5059cd598162a38412665193
Stipe Bošnjak <sbosnjak663@gmail.com>
2024-08-20 19:30:54 +0200

    Fix scrollbar outside container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <div className="bg-main-color h-screen flex">
         <div className="md:m-10 flex-1 flex gap-5">
           <Sidebar />
-          <div className="flex-1 flex flex-col gap-y-5 bg-second-color md:rounded-xl">
+          <div className="flex-1 flex flex-col gap-y-5 bg-second-color md:rounded-xl overflow-hidden">
             <Navbar />
             <Routes>
               {routes.map((route) => (
